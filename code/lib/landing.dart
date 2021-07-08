7e6300b3ed35278d584c529a65ec23064033ca33
Lina Yara Brihoum <brihoumlina@gmail.com>
2021-07-07 22:44:43 -0400

    Creating splash + landing screen

---
+++ code/lib/landing.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  @override
+  LandingScreenState createState() => LandingScreenState();
+}
+
+class LandingScreenState extends State<LandingScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.black);
+  }
+}
